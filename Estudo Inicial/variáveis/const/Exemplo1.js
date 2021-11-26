8b781da752e13dba8d26b49289c43f505096c47a
Marlon <Marlonnlmx@outlook.com>
2021-11-26 10:20:45 -0300

    Lauching code

---
+++ Estudo Inicial/variáveis/const/Exemplo1.js
@@ -0,0 +1,8 @@
+/* 
+  As variáveis do tipo 'const' não podem ser retribuídas, ou seja, não podemos atribuir um novo valor a variável criada, 
+  diferentemente das variáveis do tipo 'let' e 'var', onde o seu valor pode ser retribuído mudando assim o dado que aquela 
+  variável armazena.
+*/ 
+
+const variavel1 = 'Primeira variável'; 
+console.log(variavel1);  
