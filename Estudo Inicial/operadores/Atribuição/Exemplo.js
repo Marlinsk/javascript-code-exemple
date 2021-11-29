9dbb0de4d34758fa583ee18ae94e1be8e1bdf1a1
Marlon <Marlonnlmx@outlook.com>
2021-11-29 19:21:27 -0300

    Changes

---
+++ Estudo Inicial/operadores/Atribuição/Exemplo.js
@@ -0,0 +1,22 @@
+const valor1 = 45;
+let valor2 = 7;
+
+// valor2 = valor2 + valor1
+valor2 += valor1; 
+console.log(valor2);
+
+// valor2 = valor2 - valor1
+valor2 -= valor1; 
+console.log(valor2);
+
+// valor2 = valor2 * valor1
+valor2 *= valor1;
+console.log(valor2);
+
+// valor2 = valor2 / valor1
+valor2 /= valor1;
+console.log(valor2);
+
+// valor2 = valor2 % valor1
+valor2 %= valor1;
+console.log(valor2);
