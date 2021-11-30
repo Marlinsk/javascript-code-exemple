19bd470b09d0ca1826b4191e6b3b6436228a6aec
Marlon <Marlonnlmx@outlook.com>
2021-11-30 08:24:54 -0300

    Changes

---
+++ Estudo Inicial/variáveis/const/Exemplo.js
@@ -0,0 +1,26 @@
+/* 
+  As variáveis do tipo 'const' não podem ser retribuídas, ou seja, não podemos atribuir um novo valor a variável criada, 
+  diferentemente das variáveis do tipo 'let' e 'var', onde o seu valor pode ser retribuído mudando assim o dado que aquela 
+  variável armazena.
+*/ 
+
+const variavel1 = 'Primeira variável'; 
+console.log(variavel1);  
+
+
+// Teste de exclusividade.
+const variavel2 = 500;
+
+{
+    const variavel2 = 5;
+    console.log('Dentro=',variavel2);
+}
+
+console.log('Fora=', variavel2);
+
+function teste(){
+    const variavel2 = 'variável local';
+    console.log('Function=', variavel2);
+}
+
+teste();
