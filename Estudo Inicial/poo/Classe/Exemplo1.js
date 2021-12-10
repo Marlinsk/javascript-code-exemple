244fab8401220311f7811ad6637796b6d9afb246
Marlon <Marlonnlmx@outlook.com>
2021-12-10 07:54:44 -0300

    Launching code

---
+++ Estudo Inicial/poo/Classe/Exemplo1.js
@@ -0,0 +1,33 @@
+class lancamento {
+    constructor(nome = 'Genérico', valor = 0) {
+        this.nome = nome
+        this.valor = valor
+    }
+}
+
+class CicloFinanceiro {
+    constructor (mes, ano) {
+        this.mes = mes
+        this.ano = ano
+        this.lancamentos = []
+    }
+
+    addLancamentos(...lancamentos) {
+        lancamentos.forEach(l => this.lancamentos.push(l))
+    }
+
+    sumario() {
+        let valorConsolidado = 0
+        this.lancamentos.forEach(l => {
+            valorConsolidado += l.valor
+        })
+        return valorConsolidado
+    }
+}
+
+const salario = new lancamento('Salario', 23456)
+const contaDeLuz = new lancamento('Luz', 456)
+
+const contas = new CicloFinanceiro(6, 2018)
+contas.addLancamentos(salario, contaDeLuz)
+console.log(contas.sumario)
