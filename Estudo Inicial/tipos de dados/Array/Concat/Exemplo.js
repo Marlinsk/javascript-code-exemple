18b79e2e0610da1cbb8a494a067fd558bdf9648f
Marlon <Marlonnlmx@outlook.com>
2021-12-12 19:30:20 -0300

    Launching code

---
+++ Estudo Inicial/tipos de dados/Array/Concat/Exemplo.js
@@ -0,0 +1,6 @@
+const passaros = [ 'Elefante-Africano','Tucano-toco' ]
+const roedores = [ 'Camundongo','Esquilo-voador' ]
+const reinoAniamal = passaros.concat(roedores)
+console.log(reinoAniamal, passaros, roedores)
+
+console.log(['ab', 'wl'].concat([1, 2], [4, 6], [7, 8]))
